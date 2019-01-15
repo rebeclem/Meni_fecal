be3393efe6bdd559ef4347fef209e5c276e54c9c
Rebecca Clement <33873594+rebeclem@users.noreply.github.com>
2019-01-15 13:35:07 -0500

    Create pathoscope_human.sh

---
+++ pathoscope_human.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 05:00:00
+#SBATCH -p defq,gpu,short
+#SBATCH -o PS_hg38.%A_%a.out
+#SBATCH -e PS_hg38.%A_%a.err
+
+name=$(sed -n "$SLURM_ARRAY_TASK_ID"p ../samps.txt)
+
+#--- Start the timer
+t1=$(date +"%s")
+
+echo $name
+mkdir -p $name/human
+
+module load pathoscope
+module load bowtie2
+
+
+pathoscope.py MAP -numThreads $(nproc)\
+ -outDir $name/human \
+ -indexDir ../refs \
+ -targetIndexPrefixes genome \
+ -filterIndexPrefixes phix174,ref_prok_rep_genomes.00_ti,ref_prok_rep_genomes.01_ti,ref_prok_rep_genomes.02_ti,ref_prok_rep_genomes.03_ti,ref_prok_rep_genomes.04_ti,ref_prok_rep_genomes.05_ti,ref_viruses_rep_genomes_ti,ref_viroids_rep_genomes_ti \
+ -1 $(ls $name/flexcleaned_1.fastq) \
+ -2 $(ls $name/flexcleaned_2.fastq)
+
+ echo "Completed running PathoMAP on human for $name"
+
+
+pathoscope.py ID \
+ -outDir $name/human \
+ -thetaPrior 10000000 \
+ -alignFile $name/human/outalign.sam
+
+ echo "Completed running PathoID on human for $name"
+
+
+# this removes all unnecessary files that are taking up a lot of room.
