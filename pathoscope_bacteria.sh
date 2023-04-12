cd86aa006140625481fc58df1f88762419dbc011
Rebecca Clement <33873594+rebeclem@users.noreply.github.com>
2023-04-11 21:14:01 -0400

    new version

--- pathoscope_bacteria.sh
+++ pathoscope_bacteria.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -t 2-00:00:00
-#SBATCH -p defq,gpu,short
-#SBATCH -o PS_bac.%A_%a.out
-#SBATCH -e PS_bac.%A_%a.err
+#SBATCH -t 4:00:00
+#SBATCH -p highThru,graphical,debug,defq,small-gpu,short
+#SBATCH -o PS_hg38.%A_%a.out
+#SBATCH -e PS_hg38.%A_%a.err
 #SBATCH --array=1-25
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=rebeccaclement@gwu.edu
@@ -14,32 +14,34 @@ name=$(sed -n "$SLURM_ARRAY_TASK_ID"p ../samps.txt)
 t1=$(date +"%s")
 
 echo $name
-mkdir -p $name/bac
+mkdir -p $name/human
 
 module load pathoscope
 module load bowtie2
 
+# -targetIndexPrefixes ref_prok_rep_genomes.00_ti,ref_prok_rep_genomes.01_ti,ref_prok_rep_genomes.02_ti,ref_prok_rep_genomes.03_ti,ref_prok_rep_genomes.04_ti,ref_prok_rep_genomes.05_ti,ref_viruses_rep_genomes_ti,ref_viroids_rep_genomes_ti \
 
-pathoscope.py MAP -numThreads $(nproc)\
- -outDir $name/bac \
+pathoscope MAP -numThreads $(nproc)\
+ -outDir $name/human \
  -indexDir ../refs \
  -targetIndexPrefixes ref_prok_rep_genomes.00_ti,ref_prok_rep_genomes.01_ti,ref_prok_rep_genomes.02_ti,ref_prok_rep_genomes.03_ti,ref_prok_rep_genomes.04_ti,ref_prok_rep_genomes.05_ti,ref_viruses_rep_genomes_ti,ref_viroids_rep_genomes_ti \
- -filterIndexPrefixes genome, phix174\
+ -filterIndexPrefixes hg38full \
  -1 $(ls $name/flexcleaned_1.fastq) \
  -2 $(ls $name/flexcleaned_2.fastq)
 
- echo "Completed running PathoMAP on bac for $name"
+ echo "Completed running PathoMAP on human for $name"
 
 
-pathoscope.py ID \
- -outDir $name/bac \
+pathoscope ID \
+ -outDir $name/human \
  -thetaPrior 10000000 \
- -alignFile $name/bac/outalign.sam
+ -alignFile $name/human/outalign.sam
 
- echo "Completed running PathoID on bac for $name"
- 
- # this removes all unnecessary files that are taking up a lot of room.
-rm $name/bac/pathomap-*
+ echo "Completed running PathoID on human for $name"
+
+
+# this removes all unnecessary files that are taking up a lot of room.
+rm $name/human/pathomap-*
 
 #---Complete job
 t2=$(date +"%s")
